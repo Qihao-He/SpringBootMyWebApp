c7e4a126453f53d3da1d1a1eba1b17706d70e32e
Qihao He <qi.he@maine.edu>
2019-12-29 14:39:50 -0500

    model and view

--- src/main/java/com/telusko/demo/HomeController.java
+++ src/main/java/com/telusko/demo/HomeController.java
@@ -6,16 +6,22 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	@RequestMapping("home")
-	public String home(HttpServletRequest req, HttpServletResponse res) {
-		HttpSession session = req.getSession();
-		String name = req.getParameter("name");
-		System.out.println("in home:" + name);
-		session.setAttribute("name", name);
-		return "home";
+	public ModelAndView home(@RequestParam("name") String myName, HttpSession session) {
+//	public String home(HttpServletRequest req, HttpServletResponse res) {
+//		HttpSession session = req.getSession();
+//		String name = req.getParameter("name");
+		ModelAndView mv = new ModelAndView();
+//		System.out.println("in home: " + myName);
+//		session.setAttribute("name", myName);
+		mv.addObject("name", myName);
+		mv.setViewName("home");
+		return mv;
 	}
 }
